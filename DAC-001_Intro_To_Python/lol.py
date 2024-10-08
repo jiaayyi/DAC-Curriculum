f439d0f9c963387172a263f2485c0e5153d77b4a
jiaayyi <jiayichua.cjy@gmail.com>
2024-10-08 21:11:12 +0800

    session2

---
+++ DAC-001_Intro_To_Python/lol.py
@@ -0,0 +1,6 @@
+import math
+radius = int(input("please enter radius of circle: "))
+area = math.pi * radius ** 2
+circ = 2 * math.pi * radius
+print("The area of the circle is", area)
+print("The circumference of the circle is", circ)
